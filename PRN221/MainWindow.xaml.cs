ed08b71d10793bdfeb1d5a9bc1513f0d5436e39e
Your Name <you@example.com>
2024-01-03 20:51:21 +0700

    Add project files.

---
+++ PRN221/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PRN221
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+			txtName.Text = "Trần Văn Ngọc";
+			txtCode.Text = "HE163823";
+			txtPass.Password = "12345678";
+			txtEmail.Text = "ngoctvhe163823@fpt.edu.vn";
+			rdMale.IsChecked = true;
+			cbScholar.IsChecked = false;
+			txtDate.SelectedDate = new DateTime(2002, 01, 31);
+		}
+
+		private void Btn_Submit_Click(object sender, RoutedEventArgs e)
+		{
+			MessageBox.Show(txtName.Text);
+		}
+	}
+}
